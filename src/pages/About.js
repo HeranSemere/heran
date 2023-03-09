6efa6bc0ecbb6c9cea206568cfce9491c7b0a157
Heran <heranseme@gmail.com>
2023-03-09 11:06:52 +0300

    added setup

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,22 +2,16 @@ import logo from 'assets/grid.svg'
 
 const About = () => {
     return (
-
         <div className='full-screen'>
             <div className="side-by-side about">
                 <p className='about-text about-first'>
                     Hey, it's <span className='pink'>H</span>eran - your friendly neighborhood full-stack developer!
                 </p>
-
                 <p className='about-sub'>
                     Ready to bring your tech dreams to life? Let's team up and make some coding magic!✨
                 </p>
             </div>
-
             <img className="side-by-side profile-img" src={logo} />
-
-
-
         </div>
     )
 }
