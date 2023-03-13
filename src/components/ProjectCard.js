da50b550ea9a8dec4029844e99c625ca420254ef
Heran <heranseme@gmail.com>
2023-03-13 17:08:11 +0300

    removed bug

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -36,7 +36,7 @@ const ProjectCard = ({ slide, changeState }) => {
                 <p className="carousel-text description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore e.</p>
                 {/* <p className="carousel-text description" style={{float:'left', border:'solid 1px white', padding:'10px', width:'50px', marginRight:'5px', marginTop:'15px'}}>View</p> */}
                 <button className="carousel-button" onClick="window.location.href='https://github.com/HeranSemere/test';" style={{ float: 'left', border: 'solid 1px white', padding: '10px', marginLeft: '5px', marginTop: '15px', width: '70px' }} class="btn default">View</button>
-                <button className="carousel-button" style={{ float: 'left', border: 'solid 1px white', padding: '10px', marginLeft: '5px', marginTop: '15px', width: '70px' }} class="btn default"></button>
+                <button className="carousel-button" style={{ float: 'left', border: 'solid 1px white', padding: '10px', marginLeft: '5px', marginTop: '15px', width: '70px' }} class="btn default">Code</button>
                 {/* <p onClick={} className="carousel-text description carousel-code" style={{float:'left', border:'solid 1px white', padding:'10px', width:'50px', marginLeft:'5px', marginTop:'15px'}}>Code</p> */}
             </div>}
         </div>
